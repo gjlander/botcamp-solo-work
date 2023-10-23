d1f281e714e6f089b99abc7d2416ac65addbdc80
gjlander <garrett.jlander@gmail.com>
2023-10-23 16:17:54 +0200

    added some minor adjustments based on the lecture. has commented out buggy partial edit logic

--- travel-wishlist/app.js
+++ travel-wishlist/app.js
@@ -13,11 +13,12 @@ const updateCountriesFile = () => {
     );
 };
 
-const hasValue = (obj, value) => Object.values(obj).includes(value);
+const hasValue = (obj, value) =>
+    Object.values(obj).includes(value.toUpperCase());
 
 app.use(express.json());
 
-app.route("/countries")
+app.route("/api/countries")
     .get((req, res) => {
         const { sort, visited } = req.query;
         const sortIsOn = sort === "true";
@@ -47,35 +48,53 @@ app.route("/countries")
         return res.json(countriesList);
     })
     .post((req, res) => {
-        const { alpha2code, alpha3code } = req.body;
+        const { name, alpha2code, alpha3code } = req.body;
+        if (!name || !alpha2code || !alpha3code)
+            return res.status(400).json({ error: "Missing fields!" });
+
         const alreadyExists = countriesList.filter((country) => {
             return hasValue(country, alpha2code || alpha3code);
         }).length;
         console.log(alpha2code, alpha3code);
         console.log(alreadyExists);
+
         if (alreadyExists)
             return res.json("This country is already in the database.");
-        countriesList.push(req.body);
+
+        countriesList.push({
+            id: countriesList.length + 1,
+            name,
+            alpha2code,
+            alpha3code,
+            visited: false,
+        });
         // fs.writeFileSync(
         //     "countriesList.json",
         //     JSON.stringify(countriesList, "utf8")
         // );
         updateCountriesFile();
-        return res.json(countriesList);
+        return res.status(201).json(countriesList);
     });
 
-app.route("/countries/:code")
+app.route("/api/countries/:code")
     .get((req, res) => {
         const { code } = req.params;
         const singleCountry = countriesList.filter((country) => {
             return hasValue(country, code);
         });
-        if (!singleCountry.length) return res.json("Country not found.");
+        if (!singleCountry.length)
+            return res.status(404).json("Country not found.");
         // console.log(singleCountry);
         return res.json(singleCountry);
     })
     .put((req, res) => {
         const { code } = req.params;
+        const { name, alpha2code, alpha3code } = req.body;
+
+        if (!name || !alpha2code || !alpha3code)
+            return res
+                .status(400)
+                .json({ error: "Sorry, must edit all fields." });
         //mutates original array instead of making a copy
         const singleCountry = countriesList.filter((country) => {
             return hasValue(country, code);
@@ -83,6 +102,19 @@ app.route("/countries/:code")
         if (!singleCountry.length) return res.json("Country not found.");
         countriesList.forEach((country, i, arr) => {
             hasValue(country, code) && (arr[i] = req.body);
+
+            //attempted logic to partially edit, still buggy
+            // (arr[i] = {
+            //     id: arr[i].id,
+            //     name: !name ? arr[i].name : name,
+            //     alpha2Code: !alpha2code
+            //         ? arr[i].alpha2Code
+            //         : alpha2code.toUpperCase(),
+            //     alpha3Code: !alpha3code
+            //         ? arr[i].alpha3Code
+            //         : alpha3code.toUpperCase(),
+            //     visited: arr[i].visited,
+            // });
             //want to make it so you don't have to edit full thing
             // ({ ...arr[i], ...req.body });
         });
